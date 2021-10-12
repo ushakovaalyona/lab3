baaa484a26135ebc532939f7e7bb266b9ce3a03f
ushakovaalyona <90518585+ushakovaalyona@users.noreply.github.com>
2021-10-12 17:17:39 +0500

    Шифр Цезаря
    Написать на любом языке программу: шифрование и расшифровка текста шифром Цезаря. Пользователь вводит строку и шаг сдвига. Шаг может быть как положительным, так и отрицательным для расшифровки.

---
+++ Source.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include <string>
+#include <Windows.h>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	char mas[99] = { 'а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т',
+	'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', '€' };
+
+	cout<< "¬ведите строку на русском €зыке: ";
+	string s;
+	int n;
+	int y;
+	getline(cin, s);
+	cout << "¬ведите шаг в виде числа от -33 до 33. ƒл€ расшифровкки введите значение шага со знаком минус (например, -3): ";
+	cin >> n;
+	for (int i = 0; i < s.length(); i++) {
+		for (int j = 0; j < 33; j++) {
+			if (s[i] == mas[j]) {
+				y = (j + n) % 33;
+				if (y < 0) {
+					s[i] = mas[33 + y];
+					break;
+				}
+				else {
+					s[i] = mas[y];
+					break;
+				}
+			}
+		}
+	}
+	
+	cout << endl << s << " \n\n";
+	
+}
